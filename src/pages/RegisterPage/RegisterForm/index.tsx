51f70d0ffb79b0a05f210ba1572375732c510014
Trieu Minh Tin <mtin2@dxc.com>
2021-11-03 15:34:47 +0700

    Add goolge captcha for UI and testing purpose

--- src/pages/RegisterPage/RegisterForm/index.tsx
+++ src/pages/RegisterPage/RegisterForm/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Box, Button, Grid } from '@mui/material';
+import { Alert, Box, Button, Grid } from '@mui/material';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import registerFields from 'common/json/registerField';
@@ -8,9 +8,12 @@ import InputField from 'formControl/inputField';
 import SelectField from 'formControl/selectField';
 import { useForm } from 'react-hook-form';
 import CustomizedButtons from 'providers/buttons';
+import ReCAPTCHA from 'react-google-recaptcha';
+import styles from './styles';
 
 function RegisterForm(props: any) {
-  const { onSubmit } = props;
+  const { onSubmit, isShowAlertCaptcha, onVerifyCaptcha } = props;
+  const { styledForm } = styles;
 
   const schema = yup.object().shape({
     email: yup.string().email('You must enter a valid email').required('You must enter a valid email.'),
@@ -30,10 +33,19 @@ function RegisterForm(props: any) {
     },
   });
 
+  const onChange = (value: any) => {
+    onVerifyCaptcha(value);
+  };
+
   const { handleSubmit } = form;
 
   return (
-    <Box component='form' onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1, ml: 5, mr: 5 }}>
+    <Box component='form' onSubmit={handleSubmit(onSubmit)} noValidate sx={styledForm}>
+      {isShowAlertCaptcha && (
+        <Alert sx={{ m: '15px 0', backgroundColor: '#fdd', color: '#333' }} severity='error'>
+          The captcha you entered is incorrect. Please try again.
+        </Alert>
+      )}
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {registerFields.map(({ name, label, isRequired, type, isSelectBox, menuItems = [] }, index) => (
           <Grid item xs={4} sm={8} md={6} key={index}>
@@ -45,7 +57,10 @@ function RegisterForm(props: any) {
           </Grid>
         ))}
       </Grid>
-      <CustomizedButtons content='CREATE ACCOUNT' type='submit' variant='contained' sx={{display: 'grid', m: '30px auto'}} />
+      <div style={{ margin: '15px 0' }}>
+        <ReCAPTCHA sitekey='6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI' onChange={onChange} />
+      </div>
+      <CustomizedButtons content='CREATE ACCOUNT' type='submit' variant='contained' sx={{ display: 'grid', m: '30px auto' }} />
     </Box>
   );
 }
---
+++ src/pages/RegisterPage/RegisterForm/styles.tsx
@@ -0,0 +1,9 @@
+const styles = {
+  styledForm: {
+    mt: 1,
+    ml: 5,
+    mr: 5,
+  },
+};
+
+export default styles;
