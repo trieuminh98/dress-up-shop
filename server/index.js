9321ac49b04cca63d59b3184c5cbb86ef1526d43
Trieu Minh Tin <mtin2@dxc.com>
2021-11-08 16:55:32 +0700

    Setup JWT to authentication

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use("/api/users", userRouter);
-app.use("/api", authRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("PORT 5000 is running");
